b65cd4b2044d4381e134700626063d7d409e1cca
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-08 08:45:29 -0700

    Create async function for UI to call on

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import css directory here
 import ExchangeRate from './exchangerate.js';
+
+async function getMoney(amount) {
+  const response = await ExchangeRate.getMoney(amount);
+  if (response.ok) {
+    outputMoney(response);
+  } else {
+    outputError(response);
+  }
+}
