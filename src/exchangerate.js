bdac90028f0b38474fc3e4f8e9a9f7779d291921
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-08 08:41:26 -0700

    Process API key

--- src/exchangerate.js
+++ src/exchangerate.js
@@ -1,5 +1,17 @@
 export default class ExchangeRate {
   static async getMoney(amount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/EUR/GBP/50`);
+      const moneyResponse = await response.json();
+
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`
+        throw new Error(errorMessage);
+      }
+      return moneyResponse;
     
+    } catch(error) {
+      return error;
+    }
   }
 }
